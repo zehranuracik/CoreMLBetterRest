160705060924f04cae268e4941fff1e013da3818
Zehra Nur Acik <zehranuracik@hotmail.com>
2024-08-10 10:06:30 +0300

    BetterRest

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Zehra Nur Açık on 30.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
